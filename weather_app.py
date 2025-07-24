1039362a8eaf64a46f7ca4e62eaed5b3c11e0075
Rishab Chavadar <rchavadar@gmail.com>
2025-07-24 19:53:56 +0530

    Add files via upload

---
+++ weather_app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = "3c25276d2223bf10a8a31a0c40bf1446"
+BASE_URL = "http://api.weatherstack.com/current"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather_data = None
+    error = None
+    if request.method == 'POST':
+        city = request.form.get('city')
+        if city:
+            response = requests.get(BASE_URL, params={"access_key": API_KEY, "query": city})
+            data = response.json()
+            if "current" in data:
+                weather_data = {
+                    "city": city,
+                    "temperature": data["current"]["temperature"],
+                    "condition": data["current"]["weather_descriptions"][0]
+                }
+            else:
+                error = "City not found!"
+        else:
+            error = "Please enter a city name."
+    return render_template('weather.html', weather=weather_data, error=error)
+
+if __name__ == '__main__':
+    app.run(debug=True)
